0b63367eee713be174c49128817c206b0f51a67a
Frida <103145586+FridaTL@users.noreply.github.com>
2023-05-28 21:49:43 -0600

    Update insertB.php

--- php/insertB.php
+++ php/insertB.php
@@ -2,15 +2,13 @@
 
 include "connection.php";
 
-if( !empty($_POST["enviar"]) ) 
+
+if( !empty($_POST["btn_bio"]) ) 
 {
   if( empty( $_POST["nombre"] ) or 
-      empty( $_POST["anioN"] ) or 
-      empty( $_POST["anioM"] ) or 
-      empty( $_POST["ciudad"] ) or 
-      empty( $_POST["escuela"] ) or 
-      empty( $_POST["disciplina"] ) or 
-      empty( $_POST["obras"] ) or 
+      empty( $_POST["nacimiento"] ) or
+      empty( $_POST["escuela"] ) or
+      empty( $_POST["obras"] ) or
       empty( $_POST["elementos"] ) ) 
     {
         echo "Uno de los campos estan vacios";
@@ -19,20 +17,16 @@ if( !empty($_POST["enviar"]) )
     {
 
     $nombre = $_POST["nombre"];
-    $anioN = (int) $_POST["anioN"];
-    $anioM = (int) $_POST["anioM"];
-    $ciudad = $_POST["ciudad"]; 
-    $escuela = $_POST["escuela"]; 
-    $disciplina = $_POST["disciplina"]; 
-    $obras  = (int) $_POST["obras"];
-    $elementos = $_POST["elementos"]; 
+    $nacimiento = $_POST["nacimiento"];
+    $escuela = $_POST["escuela"];
+    $obras = $_POST["obras"];
+    $elementos = $_POST["elementos"];
 
-   
     $sql = $connection->query("insert into 
-                              biografias(nombre, anioN, anioM, ciudad, escuela, disciplina, obras, elementos)
-                              values('$nombre','$anioN','$anioM','$ciudad', '$escuela','$disciplina','$obras','$elementos')");
-    }
-
+                              biografias(nombre,nacimiento, escuela,
+                              obras, elementos)
+                              values('$nombre','$nacimiento','$escuela','$obras','$elementos')");
+  }
 }
 
 
